757c9117c9bf37709f07db8be8c1046e6b29a8b0
Diego <dj_ebest@hotmail.com>
2017-06-08 15:02:48 -0600

    Primeras vistas y funcionalidad de la pantalla principal

---
+++ app/src/main/java/pdm115/sv/fia/ues/dietaonline/IniciarSesion.java
@@ -0,0 +1,13 @@
+package pdm115.sv.fia.ues.dietaonline;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IniciarSesion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iniciar_sesion);
+    }
+}
--- app/src/main/java/pdm115/sv/fia/ues/dietaonline/MainActivity.java
+++ app/src/main/java/pdm115/sv/fia/ues/dietaonline/MainActivity.java
@@ -1,13 +1,38 @@
 package pdm115.sv.fia.ues.dietaonline;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button registrarse;
+    Button IniciarSesion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        registrarse =(Button)findViewById(R.id.botonRegistrarse);
+        registrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registrarse = new Intent(MainActivity.this, RegistrarUsuario.class);
+                startActivity(registrarse);
+            }
+        });
+
+        IniciarSesion = (Button)findViewById(R.id.botonIniciarSesion);
+        IniciarSesion.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent iniciarSesion = new Intent(MainActivity.this, IniciarSesion.class);
+                startActivity(iniciarSesion);
+            }
+            });
+
     }
 }
