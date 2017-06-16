36f42882993059ad93dc3b6b4f48b65ba2a09d80
Diego <dj_ebest@hotmail.com>
2017-06-16 05:30:22 -0600

    registrar usuario y logearse modificado

--- app/src/main/java/pdm115/sv/fia/ues/dietaonline/IniciarSesion.java
+++ app/src/main/java/pdm115/sv/fia/ues/dietaonline/IniciarSesion.java
@@ -1,13 +1,58 @@
 package pdm115.sv.fia.ues.dietaonline;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class IniciarSesion extends AppCompatActivity {
+    EditText et1, et2;
+    private Cursor fila;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iniciar_sesion);
+
+        et1 = (EditText) findViewById(R.id.etemail);
+        et2 = (EditText) findViewById(R.id.etpass);
+    }
+
+
+    public void ingresar(View v) {
+        ControladorBD admin = new ControladorBD(this, "usuario", null, 1);
+        SQLiteDatabase db = admin.getWritableDatabase();
+
+        String usuario = et1.getText().toString();
+        String contrasena = et2.getText().toString();
+        fila = db.rawQuery("select usuario,contrasena from usuarios where usuario='" + usuario + "' and contrasena='" + contrasena + "'", null);
+        //preguntamos si el cursor tiene algun valor almacenado
+        if (fila.moveToFirst() == true) {
+
+            //capturamos los valores del cursos y lo almacenamos en variable
+            String usua = fila.getString(0);
+            String pass = fila.getString(1);
+
+            //preguntamos si los datos ingresados son iguales
+            if (usuario.equals(usua) && contrasena.equals(pass)) {
+
+                //si son iguales entonces vamos a otra ventana
+
+                //Menu es una nueva actividad empty
+                Intent ven = new Intent(this, Menu.class);
+                startActivity(ven);
+
+                //limpiamos las las cajas de texto
+                et1.setText("");
+                et2.setText("");
+            }
+
+        }
     }
 }
--- app/src/main/java/pdm115/sv/fia/ues/dietaonline/RegistrarUsuario.java
+++ app/src/main/java/pdm115/sv/fia/ues/dietaonline/RegistrarUsuario.java
@@ -1,13 +1,64 @@
 package pdm115.sv.fia.ues.dietaonline;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegistrarUsuario extends AppCompatActivity {
+    EditText nombre, usuario, contraseña, email, telefono;
+    Button btnGuardarRegistro;
+
+    ControladorBD base = new ControladorBD(this, "DBDieta", null, 1);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar_usuario);
+
+
+        nombre = (EditText) findViewById(R.id.etnombre);
+        usuario = (EditText) findViewById(R.id.etusuario);
+        contraseña = (EditText) findViewById(R.id.etpass);
+        email = (EditText) findViewById(R.id.etemail);
+        telefono = (EditText) findViewById(R.id.ettel);
+
+        btnGuardarRegistro = (Button) findViewById(R.id.btnGuardarRegistro);
+        btnGuardarRegistro.setOnClickListener(new View.OnClickListener(){
+        public void onClick(View v) {
+        String nombreUsuario=nombre.getText().toString();
+        String Usuario=usuario.getText().toString();
+        String contraseñaUsuario=contraseña.getText().toString();
+        String emailUsuario=email.getText().toString();
+        String telefonoUsuario=telefono.getText().toString();
+
+            if(nombreUsuario.equals("")||Usuario.equals("")||contraseñaUsuario.equals("")||emailUsuario.equals("")||telefonoUsuario.equals(""))
+            {
+                Toast.makeText(getApplicationContext(), "Campos faltantes", Toast.LENGTH_LONG).show();
+                return;
+            }
+            else
+            {
+                // Save the Data in Database
+
+                SQLiteDatabase db = base.getWritableDatabase();
+
+                int total = 0;
+
+                if(db != null)
+                {
+                    int id = 1;
+                    String sqlInsert = "INSERT INTO usuario (id_usuario, id_rol, id_perfil, nombre, contraseña, email ) " + "VALUES (" + id + ", '" + nombreUsuario +"')";
+                    db.execSQL(sqlInsert);
+
+                    db.close();
+                }
+
+            }
+        }
+        });
     }
 }
