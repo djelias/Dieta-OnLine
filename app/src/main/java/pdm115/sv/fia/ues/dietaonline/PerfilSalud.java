9bcda1baf28ae8819271cb98e845a0cb0756c7cc
Sistema 089 <sk8jaime@hotmail.com>
2017-06-16 14:42:53 -0600

    -> PerfilUsuario

--- app/src/main/java/pdm115/sv/fia/ues/dietaonline/PerfilSalud.java
+++ app/src/main/java/pdm115/sv/fia/ues/dietaonline/PerfilSalud.java
@@ -1,11 +1,18 @@
 package pdm115.sv.fia.ues.dietaonline;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class PerfilSalud extends AppCompatActivity {
-
-    public int edadPerfil;
+    //atributos, construcotres, getters and setter del perfil de salud
+    /*public int edadPerfil;
     public float pesoPerfil;
     public String sexoPerfil;
     public float estaturaPerfil;
@@ -70,12 +77,72 @@ public class PerfilSalud extends AppCompatActivity {
 
     public void setAlergias(String alergias) {
         this.alergias = alergias;
-    }
-    
+    }*/
+
+    //campos del formulario
+    EditText edadPerfil;
+    EditText pesoPerfil;
+    CheckBox sexoPerfil;
+    EditText estaturaPerfil;
+    Spinner estadoSalud;
+    EditText alergias;
+
+    //botones del formulario
+    Button idGuardar;
+    Button idEliminar;
+    Button idActualizar;
+
+    ControladorBD base = new ControladorBD(this, "DBDieta", null, 1);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil_salud);
+
+        edadPerfil     = (EditText) findViewById(R.id.edadPerfilSalud);
+        pesoPerfil     = (EditText) findViewById(R.id.pesoPerfilSalud);
+        sexoPerfil     = (CheckBox) findViewById(R.id.checkBoxMasculino);
+        sexoPerfil     = (CheckBox) findViewById(R.id.checkBoxFemenino);
+        estaturaPerfil = (EditText) findViewById(R.id.estaturaPerfilSalud);
+        estadoSalud    = (Spinner)  findViewById(R.id.spinnerPerfilSalud);
+        alergias       = (EditText) findViewById(R.id.alergiasPerfilSalud);
+
+        idGuardar = (Button) findViewById(R.id.idGuardar);
+        idGuardar.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                int edad=Integer.parseInt(edadPerfil.getText().toString());
+                float peso=Float.parseFloat(pesoPerfil.getText().toString());
+                String sexo=sexoPerfil.getText().toString();
+                float estatura=Float.parseFloat(estaturaPerfil.getText().toString());
+                //String estado=estadoSalud
+                String alergia=alergias.getText().toString();
+
+                if(edad == 0|| peso == 0.0 || sexo.equals("") || estatura == 0 || alergia.equals(""))
+                {
+                    Toast.makeText(getApplicationContext(), "Campos faltantes", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                else
+                {
+                    // Save the Data in Database
+
+                    SQLiteDatabase db = base.getWritableDatabase();
+
+                    int total = 0;
+
+                    if(db != null)
+                    {
+                        int id = 1;
+                        String sqlInsert = "INSERT INTO PERFIL (ID_PERFIL, ID_DIETA, ID_USUARIO, EDAD, PESO_INICIO, SEXO, ESTATURA, ESTADO_SALUD, ALERGIAS ) " + "VALUES (" + id + ")";
+                        db.execSQL(sqlInsert);
+
+                        db.close();
+                    }
+                }
+            }
+        });
+
+
+
     }
 }
