b97088c7ce75a6cd6a7a31e9a2546a40f262d745
Sistema 089 <sk8jaime@hotmail.com>
2017-06-17 20:49:43 -0600

    Activity and Layout -> GestionarResultados

--- app/src/main/java/pdm115/sv/fia/ues/dietaonline/CrearEstadistica.java
+++ app/src/main/java/pdm115/sv/fia/ues/dietaonline/CrearEstadistica.java
@@ -2,12 +2,61 @@ package pdm115.sv.fia.ues.dietaonline;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 
 public class CrearEstadistica extends AppCompatActivity {
+
+
+    //campos del formulario
+    Spinner spinnerTipo;
+    EditText titulo;
+    EditText date1;
+    EditText date2;
+    Spinner spinnerEdad;
+    RadioButton RadioMasc, RadioFem;
+    Spinner spinnerPad;
+    Spinner spinnerOt;
+    EditText descripcion;
+
+    //spinners List
+    String[] tipos={"Lista de tipos", "Lineal", "Barra", "Puntos","Pastel"};
+    String[] rangos={"Lista de edades", "1-10", "11-20", "21-30","31-40", "41-50", "51-100"};
+    String[] padecimientos={"Lista de padecimientos", "Excelente", "Bueno", "Regular","Malo"};
+    String[] otros={"Lista de otros", "A", "B", "C","D"};
+
+
+    ControladorBD base = new ControladorBD(this, "DBDieta", null, 1);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_estadistica);
+
+
+        final ArrayAdapter<String> adaptador= new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,tipos);
+        spinnerTipo.setAdapter(adaptador);
+        titulo     = (EditText) findViewById(R.id.titulo);
+        date1     = (EditText) findViewById(R.id.date1);
+        date2     = (EditText) findViewById(R.id.date2);
+        final ArrayAdapter<String> adap2= new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,rangos);
+        spinnerEdad.setAdapter(adap2);
+        RadioMasc   = (RadioButton) findViewById(R.id.radioMasc);
+        RadioFem    = (RadioButton) findViewById(R.id.radioFem);
+        final ArrayAdapter<String> adap3= new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,padecimientos);
+        spinnerPad.setAdapter(adap3);
+        final ArrayAdapter<String> adap4= new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,otros);
+        spinnerOt.setAdapter(adap4);
+        descripcion = (EditText) findViewById(R.id.descripcion);
+
+
+
+
     }
+
+
 }
--- app/src/main/java/pdm115/sv/fia/ues/dietaonline/GestionarResultados.java
+++ app/src/main/java/pdm115/sv/fia/ues/dietaonline/GestionarResultados.java
@@ -1,7 +1,10 @@
 package pdm115.sv.fia.ues.dietaonline;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GestionarResultados extends AppCompatActivity {
 
@@ -9,5 +12,29 @@ public class GestionarResultados extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gestionar_resultados);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        Button btnAtras =(Button)findViewById(R.id.btnAtras);
+        btnAtras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent perfil = new Intent(GestionarResultados.this, PerfilSalud.class);
+                startActivity(perfil);
+            }
+        });
     }
 }
